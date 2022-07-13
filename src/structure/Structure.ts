7b22241c2c1570ae65a2b7c3ef9073dc8d1ed388
qfourca <qfourca3305@gmail.com>
2022-07-13 13:52:32 +0900

    optimization array parse

--- src/structure/Structure.ts
+++ src/structure/Structure.ts
@@ -1,43 +1,22 @@
 import * as THREE from 'three'
 
-
-
 export default class Structure {
     public size: THREE.Vector3
     public blockOffset: Map<number, string> = new Map()
-    public blockArrange: Array<Array<Array<number>>>
+    private blockArray:Array<number> = new Array()
+    private coefZ:number
     constructor(nbt: any) {
-        console.log(nbt.BlockData.value)
         this.size = new THREE.Vector3(nbt.Width.value, nbt.Length.value, nbt.Height.value)
+        this.coefZ = this.size.y * this.size.x
         Object.entries(nbt.Palette.value).forEach((element: any) => {
             this.blockOffset.set(element[1].value, element[0])
         })
-        this.blockOffset.forEach((value:string, key:number) => {
-            if(value.includes('command')) {
-                console.log(`${key}: ${value}`)
-            }
-            
+        this.blockArray = nbt.BlockData.value.filter((data: number) => {
+            return data > -1
         })
-
-        this.blockArrange = new Array(this.size.z).fill(0).map(() => new Array(this.size.y).fill(0).map(() => new Array(this.size.x)));
-        let counter = new THREE.Vector3(0, 0, 0)  
-        console.log(this.size)   
-        nbt.BlockData.value.forEach((element: number) => {
-            this.blockArrange[counter.z][counter.y][counter.x] = element
-            counter.x++
-            if(counter.x >= this.size.x) {
-                counter.x = 0
-                counter.y++
-            }
-            if(counter.y >= this.size.y) {
-                counter.y = 0
-                counter.z++
-            }
-            console.log('.')
-        });
-        console.log(this.blockArrange)
     }
-    at(pos:THREE.Vector3):number {
-        return this.blockArrange[pos.z][pos.y][pos.x]
+
+    in(pos:THREE.Vector3):number {
+        return this.blockArray[pos.z * this.coefZ + pos.y * this.size.x + pos.x]
     }
 }
--- src/structure/StructureRender.ts
+++ src/structure/StructureRender.ts
@@ -1,6 +1,6 @@
 import Structure from "./Structure";
 import Block from '../block/Block'
-import { Scene, Vector3, Group, Material } from "three";
+import { Scene, Vector3, Group, Material, Vector2 } from "three";
 import material from '../material'
 import mouseEvent from '../event/mouseEvent'
 import * as THREE from 'three'
@@ -8,40 +8,30 @@ export default class StructureRender {
     private structure: Structure
     private blocks: Array<Block>;
     private group:Group = new Group();
+    private chunk:Array<Vector2> = new Array()
     
     constructor(structure: Structure) {
         this.structure = structure
         this.blocks = new Array()
-        let counter: number = 10
-        this.structure.blockArrange.forEach((z:Array<Array<number>>, zIdx:number) => {
-            z.forEach((y:Array<number>, yIdx:number) => {
-                y.forEach((x:number, xIdx:number) => {
-                    if(x == 6 || x == 7) {
-                        // console.log(x)
+        console.log(this.structure.size)
+        let counter: number = 0
+        for(let z = 0; z < this.structure.size.z; z++) {
+            for(let y = 0; y < this.structure.size.y; y++) {
+                for(let x = 0; x < this.structure.size.x; x++) {
+                    const element = this.structure.in(new Vector3(x, y, z))
+                    let material
+                    if(element == 1) {
+                        material = new THREE.MeshStandardMaterial({ color: 0xffffff })
                     }
-                    else  {
-                        let material
-                        if(x == 1) {
-                            material = new THREE.MeshStandardMaterial({ color: 0xffffff })
-                        }
-                        this.blocks[counter] = new Block(new Vector3(xIdx * 10, yIdx * 10, zIdx * 10), material)
-                        this.group.add(this.blocks[counter].mesh)
-                    }
-                    // const block = x == 6 || x == 7 ? null : new Block(new Vector3(xIdx * 10, yIdx * 10, zIdx * 10))
-                    counter++
-                })
-            })
-        })
-
+                    this.blocks[counter] = new Block(new Vector3(x * 10, y * 10, z * 10), material)
+                    this.group.add(this.blocks[counter].mesh)
+                    console.log('.')
+                }
+            }
+        }
     }
     render(scene:Scene) {
         mouseEvent(document.getElementById("three-js container")!, this.group)
         scene.add(this.group)
-    }
-    rotateStrat() {
-
-    }
-    rotateStop() {
-
     }
 }
