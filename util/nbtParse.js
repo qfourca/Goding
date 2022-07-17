f11a642d29b66881616df129b17b983de5e29fe2
qfourca <qfourca3305@gmail.com>
2022-07-17 11:20:09 +0900

    imporove preporcesser

--- util/nbtParse.js
+++ util/nbtParse.js
@@ -2,14 +2,12 @@
 var fs = require('fs'),
     nbt = require('prismarine-nbt')
 
-
 fs.readFile(process.argv[2], function(error, data) {
     if(error) {
         console.log(error.message)
-    }    
+    }
     nbt.parse(data, function(error, data) {
-        console.log(data)
-        fs.writeFileSync('./home.json', JSON.stringify(data));
+        fs.writeFileSync(`./${process.argv[2].split('.')[0]}.json`, JSON.stringify(data));
     });
-    
+
 });
---
+++ util/preprocesser.js
@@ -0,0 +1,77 @@
+
+var fs = require('fs'),
+    nbt = require('prismarine-nbt')
+    io = require('fs').promises
+
+
+const infoDir = './texture/assets/minecraft/models/'
+const dataExtension = '.json'
+
+
+fs.readFile(process.argv[2], function(error, data) {
+
+    if(error) {
+        console.log(error.message)
+    }
+    nbt.parse(data, function(error, data) {
+        let result = new Object()
+        result.length = data.value.Length.value
+        result.width = data.value.Width.value
+        result.height = data.value.Height.value
+
+        console.log(data)
+        // console.log(data.value.BlockData.value)
+        result.palette = new Map()
+        Object.entries(data.value.Palette.value).forEach(element => { result.palette.set(element[1].value, { name: removeMinecraft(element[0])} )})
+        for(let i = 0; i < result.palette.size; i++) {
+            const element = result.palette.get(i)
+            element.count = data.value.BlockData.value.reduce((cnt, element) => cnt + (i === element))
+            console.log((i + 1) + ' / ' + result.palette.size)
+        }
+        result.palette = Array.from(new Map([...result.palette].sort((a, b) => a[0] - b[0])));
+        const { info, unexist } = loadAllFiles(Array.from(result.palette).map(element => 'block/' + removeOpction(element[1].name)))
+        result.info = Array.from(info)
+        result.unexist = unexist
+        fs.writeFileSync(`./${process.argv[2].split('.')[0]}.json`, JSON.stringify(result));
+        console.log("Preprocessing Success! filename: " + `${process.argv[2].split('.')[0]}.json`)
+    });
+
+});
+
+function loadAllFiles(files) {
+    let info = new Map()
+    let unexist = []
+    function loadFile(file) {
+        if(!info.has(file)) {
+            try {
+                info.set(file, (JSON.parse(fs.readFileSync(infoDir + file + dataExtension))))
+                if(info.get(file).parent != undefined) {
+                    loadFile(removeMinecraft(info.get(file).parent))
+                }
+            }
+            catch(e) {
+                if(!unexist.includes(file)) {
+                    unexist.push(file)
+                }
+            }
+        }
+    }
+    files.forEach(loadFile)
+    console.log("")
+    return { info, unexist }
+}
+
+function removeOpction(str) {
+    let tempStr = str.substring(0, str.indexOf('['))
+    if(tempStr == '') {
+        return str
+    }
+    else {
+        return tempStr
+    }
+}
+
+function removeMinecraft(str) {
+    if(str.includes("minecraft:")) return str.substring(10)
+    else return str
+}
